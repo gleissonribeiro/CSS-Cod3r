850fe8fb8a295651d8d19c497fab3056d817f928
gleissonribeiro <programminggleisson@gmail.com>
2020-10-15 18:33:49 -0300

    até display

---
+++ js/tag.js
@@ -0,0 +1,40 @@
+// ----Definição das cores----
+const colors = {
+  p: '#388e3c',
+  div: '#1565c0',
+  span:'#e53935',
+  section :'#f67809',
+  ul:'#5e35b1',
+  ol:'#fbc02d',
+  header:'#d81b60',
+  nav: '#64dd17',
+  main: '#00acc1',
+  footer: '#304ffe',
+  form: '#9f6581',
+  body: '#25b6da',
+  padrao: '#616161',
+
+  get(tag) {
+    return this[tag] ? this[tag] : this.padrao
+  }
+}
+// ---------------------------
+
+
+document.querySelectorAll('.tag').forEach(el => {
+
+  const tagName = el.tagName.toLowerCase()
+  el.style.borderColor = colors.get(tagName)
+
+  if (!el.classList.contains('nolabel')) {
+    // Cria uma tag/elemento 'label'
+    const label = document.createElement('label')
+
+    // Formata 'label' 
+    label.style.background = colors.get(tagName)
+    label.innerHTML = tagName
+
+    // Insere o elemento 'label' dentro de 'el' antes de 'el.childsNodes[0]'
+    el.insertBefore(label, el.childNodes[0])
+  }
+})
